1fbb2ab477a896cf7995032d878c0450f9257fca
Hossein <Rahmatei@gmail.com>
2022-01-02 21:37:07 +0330

    chartAndChartBar

---
+++ lib/Widget/new_transaction.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction({required this.addTx});
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final nameController = TextEditingController();
+
+  final amountController = TextEditingController();
+
+  void onSubmit() {
+    if (nameController.text.isEmpty || double.parse(amountController.text) <= 0)
+      return;
+
+    widget.addTx(nameController.text, double.parse(amountController.text));
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: nameController,
+              onSubmitted: (_) => onSubmit(),
+              //onChanged: (val) {
+              //name = val;
+              //},
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              /*onChanged: (val) {
+                        amount = val;
+                      },*/
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => onSubmit(),
+            ),
+            TextButton(
+              onPressed: onSubmit,
+              child: Text('Add Transaction'),
+              style: TextButton.styleFrom(
+                  primary: Colors.white, backgroundColor: Colors.amber),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
